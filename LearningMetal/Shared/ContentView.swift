1dcff6d5b3f40a1d75d8845213f7c6db5f0e5956
Kai Chen <xmkevinchen@users.noreply.github.com>
2022-08-20 16:25:42 -0500

    Create Xcode project

---
+++ LearningMetal/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kai Chen on 8/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Learning Metal")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningMetal/Shared/LearningMetalApp.swift
@@ -0,0 +1,18 @@
+//
+//  LearningMetalApp.swift
+//  Shared
+//
+//  Created by Kai Chen on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(width: 800, height: 600, alignment: .center)
+        }
+    }
+}
